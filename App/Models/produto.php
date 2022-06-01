890183692f2b5b5b515896bfeb12ae09ba8beb50
Yan Fonseca <yangoular@gmail.com>
2022-06-01 05:12:00 -0300

    .

--- App/Models/produto.php
+++ App/Models/produto.php
@@ -17,6 +17,22 @@ class Produto extends Model{
 	public function __set($atributo, $valor){
 		$this->$atributo = $valor;
 	}
+	public function salvarClasse(){
+		$query ="
+		BEGIN;
+			INSERT INTO classeProd(classeBase,classeDivisao,idUsuarioFkClass) VALUES(:classeBase,:classeDivisao,:idUsuarioFkClass);
+			INSERT INTO historico (nomeTablHist,codLinhaInfoHist,descHist,idUsuarioFkHist) values ('classe',last_insert_id(),'nova classe',:idUsuarioFkProd);
+			COMMIT;";
+		$stmt= $this->db->prepare($query);
+		$stmt->bindValue(':descProd',$this->__get('descProd'));
+		$stmt->bindValue(':tamProd',$this->__get('tamProd'));
+		$stmt->bindValue(':estMinProd',$this->__get('estMinProd'));
+		$stmt->bindValue(':estMaxProd',$this->__get('estMaxProd'));
+		$stmt->bindValue(':classeProd',$this->__get('classeProd'));
+		$stmt->bindValue(':idUsuarioFkProd',$this->__get('idUsuario'));
+		$stmt->execute();
+		return $this;
+	}
 	public function salvarProd(){
 		$query ="
 		BEGIN;
